2e04920c7cd60a487cbd4f05b94e18b6994c4ff8
Shankar Kulumani <shanks.k@gmail.com>
2017-08-12 10:21:57 -0400

    Add some more example code

---
+++ ch3/chartype.cpp
@@ -0,0 +1,12 @@
+// the char type
+#include <iostream>
+int main ()
+{
+    char ch;
+
+    std::cout << "Enter a character: " << std::endl;
+    std::cin >> ch;
+    std::cout << "Holla! " ;
+    std::cout << "Thank you for the " << ch << " character." << std::endl;
+    return 0;
+}
--- ch3/exceed.cpp
+++ ch3/exceed.cpp
@@ -6,4 +6,24 @@
 int main()
 {
     short sam = SHRT_MAX;
+    unsigned short sue = sam;
+
+    std::cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    std::cout << " dollars deposited." << std::endl
+              << "Add $1 to each account." << std::endl << "Now ";
+    sam = sam + 1;
+    sue = sue + 1;
+    std::cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    std::cout << " dollars deposited.\nPoor Sam!" << std::endl;
+    sam = ZERO;
+    sue = ZERO;
+    std::cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    std::cout << " dollars deposited." << std::endl;
+    std::cout << "Take $1 from each account." << std::endl << "Now ";
+    sam = sam - 1;
+    sue = sue - 1;
+    std::cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    std::cout << " dollars deposited." << std::endl << "Lucky, Sue!" << std::endl;
+    return 0;
+}
 
