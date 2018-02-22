d5dec60781dab7350b6815f648c9e57f24eea9d3
Shankar Kulumani <shanks.k@gmail.com>
2018-02-22 15:41:02 -0500

    Finsished ch1

--- ch1/bookstore.cpp
+++ ch1/bookstore.cpp
@@ -1,12 +1,31 @@
 // Bookstore problem from the chapter
 #include <iostream>
+#include <Sales_item.h>
 
 int main()
 {
-    std::cout << "Enter two numbers: " << std::endl;
-    int v1 = 0, v2 = 0;
-    std::cin >> v1 >> v2;
-    std::cout << "The sum of " << v1 << " and " << v2
-              << " is " << v1 + v2 << std::endl;
+    Sales_item total;
+    if (std::cin >> total)
+    {
+        Sales_item trans;
+        while (std::cin >> trans)
+        {
+            if (total.isbn() == trans.isbn())
+            {
+                total += trans; 
+            }
+            else
+            {
+                std::cout << total << std::endl;
+                total = trans;
+            }
+        }
+        std::cout << total << std::endl;
+    }
+    else
+    {
+        std::cerr << "No data?" << std::endl;
+        return -1;
+    }
     return 0;
 }
