7580d9d7ba8def6e73b015b8a876de78a1ea0dc8
Shankar Kulumani <shanks.k@gmail.com>
2018-02-22 15:23:42 -0500

    Add anothe exercise

---
+++ ch1/ex1-20.cpp
@@ -0,0 +1,13 @@
+// Read some book sales and print to std out
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+    Sales_item book;
+    while (std::cin >> book)
+    {
+        std::cout << book << std::endl;
+    }
+    return 0;
+}
