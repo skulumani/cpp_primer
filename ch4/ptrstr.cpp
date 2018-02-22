e08a6db4d9a68f8f38736725bc2eba01186ed9ac
Shankar Kulumani <shanks.k@gmail.com>
2018-02-21 22:16:38 -0500

    Using pointers and s trings

--- ch4/ptrstr.cpp
+++ ch4/ptrstr.cpp
@@ -10,6 +10,24 @@ int main()
 
     std::cout << animal << " and ";
     std::cout << bird << "\n";
-    // std::cout << ps << "\n";
+    /* std::cout << ps << "\n"; // this will cause problems since ps is not pointing to anythign */
+    
+    std::cout << "Enter a kind of animal: ";
+    std::cin >> animal; // only okay if input is less than 20 chars
+    // std::cin >> ps; // ps does not point to an allocated space
+    //
+
+    ps = animal; // ps points to a string
+    std::cout << ps << "s!\n";
+    std::cout << "Before using strcpy():\n";
+    std::cout << animal << " at " << (int *) animal << std::endl;
+    std::cout << ps << " at " << (int *) ps << std::endl;
+
+    ps = new char[strlen(animal) + 1]; // allocate new storage
+    strcpy (ps, animal); // copy string to new storage
+    std::cout << "After using strcpy():\n";
+    std::cout << animal << " at " << (int *) animal << std::endl;
+    std::cout << ps << " at " << (int *) ps << std::endl;
+    delete [] ps;
     return 0;
 }
