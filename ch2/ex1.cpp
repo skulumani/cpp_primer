364400f999d00ba6421f375a5897da65ed527d48
Shankar Kulumani <shanks.k@gmail.com>
2018-02-22 15:01:30 -0500

    Delete the code from the wron gbook

--- ch2/ex1.cpp
+++
@@ -1,9 +0,0 @@
-// print my name and address
-#include <iostream>
-
-int main()
-{
-    std::cout << "My name is Shankar Kulumani" << std::endl;
-    std::cout << "My address: 123 Fake street" << std::endl
-              << " Fake city, WA 12455" << std::endl;
-}
